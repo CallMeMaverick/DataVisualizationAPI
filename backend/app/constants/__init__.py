daf23e8697bcff5433a96946ddfb29083351a539
«Sviatoslav» <ksvyat21@gmail.com>
2024-09-28 12:40:26 +0200

    feat(consts): file errors
    Added errors for files

--- backend/app/constants/__init__.py
+++ backend/app/constants/__init__.py
@@ -1,5 +1,6 @@
 from .routers import file_router
 from .validation import ALLOWED_EXTENSIONS
+from .errors import file_errors
 
 
-__all__ = ["file_router", "ALLOWED_EXTENSIONS"]
+__all__ = ["file_router", "ALLOWED_EXTENSIONS", "file_errors"]
---
+++ backend/app/constants/errors.py
@@ -0,0 +1,5 @@
+file_errors = {
+    "BAD_JSON": "Could not parse JSON",
+    "BAD_CSV": "CSV file is empty",
+    "BAD_REQUEST": "Could not process"
+}
