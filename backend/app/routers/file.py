2eb5f72874b10dfd64a931a91c9bd683749bea6f
«Sviatoslav» <ksvyat21@gmail.com>
2024-09-28 14:26:00 +0200

    feat(file): change file processing logic
    Return columns when file is uploaded to pre-configure the chart

--- backend/app/routers/file.py
+++ backend/app/routers/file.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, UploadFile
 from app.constants.routers import file_router
 from app.services.file import FileService
-from fastapi.responses import StreamingResponse
 
 
 router = APIRouter(prefix=file_router["prefix"])
@@ -9,6 +8,4 @@ router = APIRouter(prefix=file_router["prefix"])
 
 @router.post(file_router["post"])
 async def upload_file(file: UploadFile):
-    img_io = await FileService.process_file(file)
-    return StreamingResponse(img_io, media_type="image/png")
-
+    return await FileService.process_file(file)
--- backend/app/services/file.py
+++ backend/app/services/file.py
@@ -1,31 +1,44 @@
+from typing import Dict
 from fastapi import UploadFile, HTTPException
 from app.utils.file import validate_extension, process_json, process_csv
-import pandas as pd
+from app.constants.errors import file_errors
 
 
 class FileService:
+    """
+    A service for processing uploaded files in various formats.
+
+    This class provides static methods for processing
+    files such as CSV and JSON. It returns relevant data (e.g., column names)
+    extracted from the processed file.
+
+    Methods:
+        process_file(file: UploadFile) -> Dict[str, list[str]]:
+            Validates and processes the given file based on its type (CSV or JSON)
+            and returns the list of columns.
+    """
+
     @staticmethod
-    async def process_file(file: UploadFile) -> pd.DataFrame:
+    async def process_file(file: UploadFile) -> Dict[str, list[str]]:
         """
         Processes the uploaded file based on its type
         :param file: File to process
         :return: Processed data (as a DataFrame for CSV or dict for JSON)
         """
 
-        file_extension = file.filename.split('.')[-1].lower()
+        file_extension = file.filename.split('.')[-1]
         validate_extension(file_extension)
 
         file_content = await file.read()
-
-        if file_extension == "json":
-            data = process_json(file_content)
-        elif file_extension == "csv":
-            data = process_csv(file_content)
+        if file.content_type == "text/csv":
+            df = process_csv(file_content)
+        elif file.content_type == "application/json":
+            df = process_json(file_content)
         else:
             raise HTTPException(
                 status_code=400,
-                detail="Could not process file's extension"
+                detail=file_errors["BAD_REQUEST"]
             )
 
-        return data
-
+        columns = df.columns.tolist()
+        return {"columns": columns}
